419103c78a94bb15fafce6c1021568a0c08f0227
LD101 <jmuendongei@gmail.com>
2023-07-31 11:29:46 +0300

    Project_Backup

---
+++ Assets/Scripts/Hit_Points.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "HitPoints")]
+public class Hit_Points : MonoBehaviour
+{
+    public float value;
+}
